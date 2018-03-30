2e6ff0573b125809acebe40c0960cf6357b13437
E166294E <admin>
2018-03-30 09:43:49 +0200

    Fenêtre modale fonctionnelle

--- Projet/js/main.js
+++ Projet/js/main.js
@@ -40,80 +40,82 @@ $(document).ready(function(){
 
   form.submit(function(e){
     e.preventDefault();
-  if (isSelected && search != "") {
-    var input = search;
-    var perPage = $('input[name="nbResults"]').val();
-    console.log(input +" "+perPage);
-    if (perPage == 0) {
-      perPage = 100;
-    }
+    if (isSelected && search != "") {
+      var input = search;
+      var perPage = $('input[name="nbResults"]').val();
+      console.log(input +" "+perPage);
+      if (perPage == 0) {
+        perPage = 100;
+      }
 
-    $.ajax({
-      url : 'https://api.flickr.com/services/rest/',
-      type : 'GET',
-      data : {
+      $.ajax({
+        url : 'https://api.flickr.com/services/rest/',
+        type : 'GET',
+        data : {
           method : "flickr.photos.search",
           api_key : "044417fb5b28b6ccb072373638d89bd4",
           tags : input,
           format : "json",
           nojsoncallback : "1",
           per_page : perPage
-      },
-      success : function(codeHtmlSucces, statut){
-        console.log("succès");
-      },
-      error : function(resultat, statut, erreur){
-        console.log(erreur);
-      }
-    }).done(function(data){
-      var photos = data.photos.photo;
-      $(".body").empty();
-      if (photos.length != 0) {
-        $('#NoCityFound').dialog().dialog("close");
-        $('#NoCityFound').css("display", "none");
-        $.each(photos, function(i,data){
-          liste.append("<li><img src =https://farm"+data.farm+".staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+".jpg></li>");
-        });
-        var photo = $('img');
-        $.each(photo, function(index, photos){
+        },
+        success : function(codeHtmlSucces, statut){
+          console.log("succès");
+        },
+        error : function(resultat, statut, erreur){
+          console.log(erreur);
+        }
+      }).done(function(data){
+        //on trouve dans photos les infos de toutes les images retournées par la requête
+        var photos = data.photos.photo;
+        $(".body").empty();
+        if (photos.length != 0) {
+          $('#NoCityFound').dialog().dialog("close");
+          $('#NoCityFound').css("display", "none");
+          //Sur chacune des "images infos on créer une balise image"
           console.log(photos);
-          photo.click(function(){
-            console.log(" test");
-          $.ajax({
-            url : 'https://api.flickr.com/services/rest/',
-            type : 'GET',
-            format : "json",
-            nojsoncallback : "1",
-            data : {
-              method : "flickr.photos.getInfo",
-              api_key : "044417fb5b28b6ccb072373638d89bd4",
-              photo_id : photo.id,
-              secret : photo.secret
-            },
-            success : function(codeHtmlSucces, statut){
-              console.log(data);
-              console.log("succès");
-            },
-            error : function(resultat, statut, erreur){
-              console.log(erreur);
-            }
-          }).done(function(data){
-            console.log(data);
-            $('#infosPhoto').append("<p>"+data+"</p>");
-            $('#infosPhoto').css("display", "block");
-            $('#infosPhoto').dialog().dialog("open");
+          $.each(photos, function(i,data){
+            liste.append("<li><img class='images' id ='image"+i+"' src =https://farm"+data.farm+".staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+".jpg></li>");
+            var test = $("#image"+i);
+            console.log(test);
+            console.log(i);
+            test.click(function(){
+              var ajax = $.ajax({
+                url : 'https://api.flickr.com/services/rest/',
+                type : 'GET',
+                format : "json",
+                nojsoncallback : "1",
+                data : {
+                  method : "flickr.photos.getInfo",
+                  format : "json",
+                  api_key : "044417fb5b28b6ccb072373638d89bd4",
+                  nojsoncallback : "1",
+                  photo_id : data.id,
+                  secret : data.secret
+                },
+                success : function(codeHtmlSucces, statut){
+                  console.log("succès");
+                },
+                error : function(resultat, statut, erreur){
+                  console.log(erreur);
+                }
+              }).done(function(data){
+                var owner = data.photo.owner;
+                $('#infosPhoto').append("<p> Endroit de la photo"+owner.location+"</p>");
+                $('#infosPhoto').append("<p> Nom : "+data.photo.title._content+"</p>");
+                $('#infosPhoto').append("<p> Photographe : "+owner.username+"</p>");
+                $('#infosPhoto').css("display", "block");
+                $('#infosPhoto').dialog().dialog("open");
+              });
+            });
           });
-        });
+        }
+        else{
+          $('#NoCityFound').css("display", "block");
+          $('#NoCityFound').dialog().dialog("open");
+        }
       });
-
-      }
-      else{
-        $('#NoCityFound').css("display", "block");
-        $('#NoCityFound').dialog().dialog("open");
-      }
-    });
-
-  }else{
-    alert("Veuillez choisir une ville");
-  }});
-});
+    }else{
+      alert("Veuillez choisir une ville");
+    }});
+  });
